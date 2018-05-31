11f444880cc99715c537ad9eff733420d3627fdf
MirkoRima <mirko.rima94@gmail.com>
2018-05-31 09:15:20 +0200

    string number removed

--- Code/Data_Filtering.py
+++ Code/Data_Filtering.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import nltk
 import string
+import re
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
@@ -11,7 +12,10 @@ Dataset_Hotels = pd.read_csv("../Datasets/Hotels.csv")
 stop = stopwords.words("english") + list(string.punctuation)
 #Reviews = Dataset_Reviews["Content"]
 
-Reviews = pd.DataFrame("me and MY FRIEND, Mirko are going. to fuck! ourselves", index=range(1), columns=['A'])
+text = "me and 999999 MY FRIEND, Mirko are going. 11 to fuck! ourselves"
+print(text)
+
+Reviews = pd.DataFrame(re.sub(r'\d+', '', text), index=range(1), columns=['A'])
 
 print(Reviews)
 
--- README.md
+++ README.md
@@ -3,10 +3,14 @@
 Using naive bayes to predict overall ratings in tripadvisor reviews.
 
 ### Group Members
--Prete Francesco  
--Spereafico Andrea  
--Rima Mirko  
+
+Group components:
+-Prete Francesco
+-Spereafico Andrea
+-Rima Mirko
 
 ### License
 
 If you are not MPD's 2017/18 students at University of Milano Bicocca, fell free to use it.
+
+test
